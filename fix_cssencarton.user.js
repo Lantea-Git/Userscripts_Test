121d9653bd8d754abf5396104add566eebd39a94
Lantea-Git <184443362+Lantea-Git@users.noreply.github.com>
2025-07-19 23:47:23 +0200

    Create fix_cssencarton.user.js

---
+++ fix_cssencarton.user.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name         csspatchnotune
+// @namespace    csspatchnotune
+// @version      0.0.2
+// @description  Restore le bouton checkbox.
+// @match        *://nocturnex.alwaysdata.net/mosajax/
+// @grant        GM_addStyle
+// @downloadURL  https://github.com/Lantea-Git/Userscripts_Test/raw/main/fix_cssencarton.user.js
+// @updateURL    https://github.com/Lantea-Git/Userscripts_Test/raw/main/fix_cssencarton.user.js
+// ==/UserScript==
+
+
+GM_addStyle(`
+    .prev .grille div {
+        width: 66px;
+        height: 49px;
+        display: inline-block;
+        box-sizing: border-box;
+        margin: 0;
+        border: 1px solid black;
+    }
+`);
